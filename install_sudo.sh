bc685370a9c5b380c3db3ee89b58ca44cccb0ff1
Ben Wolthuis <ben.wolthuis@gmail.com>
2019-02-19 16:29:55 +0100

    added more settings in vimrc, experimental install script

---
+++ install_sudo.sh
@@ -0,0 +1,6 @@
+sudo apt install i3 cmus feh arandr vim curl git
+sudo cp i3config /etc/i3/config
+sudo cp i3status.conf /etc/i3status.conf
+sudo cp bashrc ~/.bashrc
+sudo cp bash_aliases ~/.bash_aliases 
+sudo cp vimrc /etc/vim/vimrc
