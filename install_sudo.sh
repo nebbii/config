e6fc6c5b7a923f9aff65090f3d8166bcc2b8cc5a
Ben Wolthuis <ben.wolthuis@gmail.com>
2019-02-19 16:33:08 +0100

    fixed install script

--- install_sudo.sh
+++ install_sudo.sh
@@ -1,6 +1,9 @@
-sudo apt install i3 cmus feh arandr vim curl git
-sudo cp i3config /etc/i3/config
+sudo apt install i3 cmus feh arandr vim curl git screenfetch 
+sudo cp i3config ~/.config/i3/config
 sudo cp i3status.conf /etc/i3status.conf
 sudo cp bashrc ~/.bashrc
 sudo cp bash_aliases ~/.bash_aliases 
-sudo cp vimrc /etc/vim/vimrc
+sudo cp vimrc ~/.vimrc
+
+git config --global user.email "ben.wolthuis@gmail.com"
+git config --global user.name "Ben"
