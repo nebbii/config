e0890086d495c3a957eda575d7f0a5c8f740c01c
Ben <ben.wolthuis@gmail.com>
2019-04-18 16:49:47 +0200

    added libraries for dumb stuff

--- install_sudo.sh
+++ install_sudo.sh
@@ -1,6 +1,9 @@
 # install programs
 # missing: google-chrome, discord, slack, phpstorm etc
-sudo apt install i3 cmus feh arandr vim curl git screenfetch compton thunderbird kolourpaint pavucontrol 
+sudo apt-get -y install i3 cmus feh arandr vim curl git screenfetch compton thunderbird kolourpaint pavucontrol 
+
+# libraries for dumb stuff
+sudo apt-get -y install libev-dev libxcb-composite0 libxcb-composite0-dev libxcb-xinerama0 libxcb-randr0 libxcb-xinerama0-dev libxcb-xkb-dev libxcb-image0-dev libxcb-util-dev libxkbcommon-x11-dev libjpeg-turbo8-dev libpam0g-dev libfftw3-dev libncursesw5-dev cmake libpulse-dev
 
 # vim plugins
 git submodules init
