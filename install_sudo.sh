4fda3805f7cfcce0baecae6d40d4ee7d18090cd0
Ben <ben.wolthuis@gmail.com>
2019-11-29 11:23:00 +0100

    Updated vimrc for vundle

--- install_sudo.sh
+++ install_sudo.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # install programs
 # missing: google-chrome, discord, slack, phpstorm etc
 sudo apt-get -y install i3 cmus feh arandr vim curl git screenfetch compton thunderbird kolourpaint pavucontrol redshift ctags cscope ibus-anthy
@@ -8,10 +10,12 @@ sudo apt-get -y install libev-dev libxcb-composite0 libxcb-composite0-dev libxcb
 git submodules update --init
 
 # vim plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d "~/.vim/bundle/Vundle.vim" ] ; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 # dotfiles
-mkdir ~/.config/i3
+mkdir -p ~/.config/i3
 sudo cp i3config ~/.config/i3/config
 sudo cp i3status.conf /etc/i3status.conf
 sudo cp bashrc ~/.bashrc
